a29b30de5fccdfe6bdb263456ca23c6e246f14eb
sabrinacinque <cinque.sabrina@gmail.com>
2024-08-01 10:54:50 +0200

    commcomm

--- InFornoWebApp/Models/Product.cs
+++ InFornoWebApp/Models/Product.cs
@@ -8,15 +8,15 @@ namespace InFornoWebApp.Models
         public int  Id { get; set; }
 
       
-        public string ? Name { get; set; }
+        public string  Name { get; set; }
 
         public string ? PhotoUrl { get; set; }
 
         
-        public decimal ? Price { get; set; }
+        public decimal  Price { get; set; }
 
        
-        public int ? DeliveryTime { get; set; } // Time in minutes
+        public int ? DeliveryTime { get; set; } // tempo in minuti
 
         public List<ProductIngredient> ?  ProductIngredients { get; set; } = new List<ProductIngredient>();
     }
